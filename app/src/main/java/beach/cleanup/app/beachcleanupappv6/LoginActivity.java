a0b2d0308c5a4895f8180663d24150a59515bfee
Jcp2k <justinpound23@gmail.com>
2024-09-08 14:36:06 +1200

    Finished Login and Signup Just needs debugging to fix crashing issues plus End User Feedback.

--- app/src/main/java/beach/cleanup/app/beachcleanupappv6/LoginActivity.java
+++ app/src/main/java/beach/cleanup/app/beachcleanupappv6/LoginActivity.java
@@ -1,5 +1,6 @@
 package beach.cleanup.app.beachcleanupappv6;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -17,18 +18,37 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_login);
-
+        setContentView(binding.getRoot()); // Use View Binding
 
         databaseHelper = new DatabaseHelper(this);
+
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = binding.loginEmail.getText().toString();
-                String password= binding.loginPassword.getText().toString();
+                String password = binding.loginPassword.getText().toString();
 
                 if (email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "Please Enter Email and Password", Toast.LENGTH_SHORT).show();
+                } else {
+                    boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+
+                    if (checkCredentials) {
+                        Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
             }
         });
     }

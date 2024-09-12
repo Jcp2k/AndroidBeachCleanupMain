9558d9f361b6ce4f5ecef5e3f4de182fdd0dcabb
Jcp2k <justinpound23@gmail.com>
2024-09-13 10:30:17 +1200

    Finished Popup Page with Static Information and continued Debugging Signup/Login Page. Need to implement data into static information for both markers.

--- app/src/main/java/beach/cleanup/app/beachcleanupappv6/MyInfoWindowAdapter.java
+++ app/src/main/java/beach/cleanup/app/beachcleanupappv6/MyInfoWindowAdapter.java
@@ -2,7 +2,8 @@ package beach.cleanup.app.beachcleanupappv6;
 
 import android.view.LayoutInflater;
 import android.view.View;
-
+import android.content.Context;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -17,6 +18,9 @@ public class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
         this.context = context;
     }
 
+    public MyInfoWindowAdapter(MapFragment mapFragment) {
+    }
+
     @Nullable
     @Override
     public View getInfoContents(@NonNull Marker marker) {
@@ -26,9 +30,11 @@ public class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     @Nullable
     @Override
     public View getInfoWindow(@NonNull Marker marker) {
-        View infoView = LayoutInflater.from(context).inflate(R.layout.custom_info, null)
-
-
+        View infoView = LayoutInflater.from(context).inflate(R.layout.custom_info, null);
+        TextView title = infoView.findViewById(R.id.title);
+        TextView snipp = infoView.findViewById(R.id.snipp);
+        title.setText(marker.getTitle());
+        snipp.setText(marker.getSnippet());
 
         return infoView;
     }
